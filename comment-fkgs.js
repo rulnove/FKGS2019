ae6e17df36782f8b15f908e73989c1eba6fda29a
rulnove <rulid06@gmail.com>
2019-02-25 19:10:10 +0700

    Add files via upload

---
+++ comment-fkgs.js
@@ -0,0 +1,5 @@
+//<![CDATA[
+function downloadJSAtOnload(){var d=document.createElement("script");d.src="URL HOSTING JAVASCRPT DI SINI",document.body.appendChild(d)}window.addEventListener?window.addEventListener("load",downloadJSAtOnload,!1):window.attachEvent?window.attachEvent("onload",downloadJSAtOnload):window.onload=downloadJSAtOnload;
+//]]>
+</script>
+</b:if>
